477fb6ce948281843c09679bcf5ecc877f5f7a48
grishuchkov <gri.d.a@ya.ru>
2023-08-05 21:49:18 +0300

    add: getSize(), getRandomCoordinates(), getEmptyRandomCoordinates.

--- src/main/java/org/simulation/Map.java
+++ src/main/java/org/simulation/Map.java
@@ -3,13 +3,12 @@ package org.simulation;
 import org.simulation.entities.Entity;
 
 import java.util.HashMap;
+import java.util.Random;
 
 public class Map {
-    private final int MAX_X = 10;
-    private final int MAX_Y = 10;
+    private final int MAX_X = 20;
+    private final int MAX_Y = 15;
     private HashMap<Coordinates, Entity> entityStorage = new HashMap<>();
-
-
 
     public int getMaxCoordinate(Axes axis){
         if (axis.equals(Axes.X)){
@@ -17,6 +16,26 @@ public class Map {
         }else {
             return MAX_Y;
         }
+    }
+
+    public Coordinates getEmptyRandomCoordinates(){
+        while (true){
+            Coordinates  coordinates = getRandomCoordinates();
+            if(isEmpty(coordinates)){
+                return coordinates;
+            }
+        }
+    }
+
+    private Coordinates getRandomCoordinates(){
+        Random random = new Random();
+        int x = random.nextInt(MAX_X) + 1;
+        int y = random.nextInt(MAX_Y) + 1;
+        return new Coordinates(x,y);
+    }
+
+    public int getSize(){
+        return MAX_X * MAX_Y;
     }
 
     public void setEntity(Entity entity, Coordinates coordinates){
