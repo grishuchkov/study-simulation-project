7c915180abd1b4ae8e7f09616d7417495d2e8c72
grishuchkov <gri.d.a@ya.ru>
2023-08-05 15:15:31 +0300

    add: sprite render for entities

--- src/main/java/org/simulation/Renderer.java
+++ src/main/java/org/simulation/Renderer.java
@@ -1,16 +1,43 @@
 package org.simulation;
 
-import java.util.HashMap;
+import org.simulation.entities.Entity;
 
 public class Renderer {
 
-    public void render(Map map){
+    Map map;
+    public Renderer(Map map) {
+        this.map = map;
+    }
 
+    public void render(){
         for (int y = 1; y <= map.getMaxCoordinate(Axes.Y); y++) {
             for (int x = 1; x <= map.getMaxCoordinate(Axes.X); x++){
-                System.out.print(x + " ");
+                Coordinates coordinates = new Coordinates(x,y);
+                String entitySprite = " ";
+                if(!map.isEmpty(coordinates)){
+                    entitySprite = setEntitySprite(map.getEntity(coordinates));
+                }
+                System.out.print(entitySprite);
             }
             System.out.println();
+        }
+    }
+
+    private String setEntitySprite(Entity entity){
+        String type = entity.getClass().getSimpleName();
+        switch (type){
+            case "Rock":
+                return "\uD83E\uDEA8";
+            case "Grass":
+                return "\uD83C\uDF31";
+            case "Tree":
+                return "\uD83C\uDF33";
+            case "Herbivore":
+                return "\uD83D\uDC24";
+            case "Predator":
+                return "\uD83E\uDD8A";
+            default:
+                return " ";
         }
     }
 }
--- src/main/java/org/simulation/Simulation.java
+++ src/main/java/org/simulation/Simulation.java
@@ -3,8 +3,9 @@ package org.simulation;
 public class Simulation {
     public static void main(String[] args) {
         Map worldMap = new Map();
-        Renderer consoleRenderer = new Renderer();
+        Renderer consoleRenderer = new Renderer(worldMap);
 
-        consoleRenderer.render(worldMap);
+
+        consoleRenderer.render();
     }
 }
