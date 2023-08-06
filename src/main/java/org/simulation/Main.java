7206ab450b9fffb55d20f5a716fb78cf24edfa09
grishuchkov <gri.d.a@ya.ru>
2023-08-06 14:01:52 +0300

    chore: - replace fields from method to class - add all SpawnActions to initActions - rename method: start -> test

--- src/main/java/org/simulation/Main.java
+++ src/main/java/org/simulation/Main.java
@@ -3,6 +3,6 @@ package org.simulation;
 public class Main {
     public static void main(String[] args) {
         Simulation simulation = new Simulation();
-        simulation.start();
+        simulation.test();
     }
 }
--- src/main/java/org/simulation/Simulation.java
+++ src/main/java/org/simulation/Simulation.java
@@ -1,28 +1,31 @@
 package org.simulation;
 
 import org.simulation.action.Action;
-import org.simulation.action.spawn.PredatorSpawnAction;
-import org.simulation.action.spawn.RockSpawnAction;
+import org.simulation.action.spawn.GrassSpawnAction;
+import org.simulation.action.spawn.creature.HerbivoreSpawnAction;
+import org.simulation.action.spawn.creature.PredatorSpawnAction;
+import org.simulation.action.spawn.statics.RockSpawnAction;
+import org.simulation.action.spawn.statics.TreeSpawnAction;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class Simulation {
-
-    public void start(){
-        Map worldMap = new Map();
-        Renderer consoleRenderer = new Renderer(worldMap);
-
+    private final Map worldMap = new Map();
+    private final Renderer consoleRenderer = new Renderer(worldMap);
+    public void test(){
         initAction(worldMap);
         consoleRenderer.render();
     }
-
 
     private void initAction(Map map){
         List<Action> initActions = new LinkedList<>();
 
         initActions.add(new RockSpawnAction(map));
+        initActions.add(new TreeSpawnAction(map));
+        initActions.add(new GrassSpawnAction(map));
         initActions.add(new PredatorSpawnAction(map));
+        initActions.add(new HerbivoreSpawnAction(map));
 
         for (int i = 0; i < initActions.size(); i++) {
             initActions.get(i).perform();
