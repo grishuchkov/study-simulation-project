1b194840b803557a821555ba9f1c44b46ef12cb9
grishuchkov <gri.d.a@ya.ru>
2023-08-06 13:37:25 +0300

    refactor: replace Main method into Main.java from Simulation.java

--- src/main/java/org/simulation/Simulation.java
+++ src/main/java/org/simulation/Simulation.java
@@ -1,17 +1,31 @@
 package org.simulation;
 
 import org.simulation.action.Action;
-import org.simulation.action.RockSpawnAction;
+import org.simulation.action.spawn.PredatorSpawnAction;
+import org.simulation.action.spawn.RockSpawnAction;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Simulation {
-    public static void main(String[] args) {
+
+    public void start(){
         Map worldMap = new Map();
         Renderer consoleRenderer = new Renderer(worldMap);
 
-
-        Action rockSpawnAction = new RockSpawnAction(worldMap);
-        rockSpawnAction.perform();
-
+        initAction(worldMap);
         consoleRenderer.render();
+    }
+
+
+    private void initAction(Map map){
+        List<Action> initActions = new LinkedList<>();
+
+        initActions.add(new RockSpawnAction(map));
+        initActions.add(new PredatorSpawnAction(map));
+
+        for (int i = 0; i < initActions.size(); i++) {
+            initActions.get(i).perform();
+        }
     }
 }
