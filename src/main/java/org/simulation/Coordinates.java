ffc501bb79ea0459cbfe8f25e46c31b28f97c5c2
grishuchkov <gri.d.a@yandex.ru>
2023-08-27 18:34:49 +0300

    fix: delete ENUM with axes, replace on fields. (forgot commit file)

--- src/main/java/org/simulation/Coordinates.java
+++ src/main/java/org/simulation/Coordinates.java
@@ -1,12 +1,20 @@
 package org.simulation;
 
 public class Coordinates {
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
     public Coordinates(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     @Override
@@ -25,5 +33,13 @@ public class Coordinates {
         int result = x;
         result = 31 * result + y;
         return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinates{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
     }
 }
