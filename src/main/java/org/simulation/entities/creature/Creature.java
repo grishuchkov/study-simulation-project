fd377482ebda1c2c52577ee62a1638f5848302fe
grishuchkov <gri.d.a@yandex.ru>
2023-08-30 11:21:41 +0300

    add: general logic of movement

--- src/main/java/org/simulation/entities/creature/Creature.java
+++ src/main/java/org/simulation/entities/creature/Creature.java
@@ -1,18 +1,67 @@
 package org.simulation.entities.creature;
 
-import org.simulation.AStarPathSearch;
+import org.simulation.AStarPathSearcher;
 import org.simulation.Coordinates;
 import org.simulation.Map;
+import org.simulation.PathSearcher;
 import org.simulation.entities.Entity;
+
+import java.util.List;
+import java.util.Random;
 
 public abstract class Creature extends Entity {
 
+    private int hp;
+    protected int speed;
+
+    private final PathSearcher pathSearch = new AStarPathSearcher();
+    protected final Random random = new Random();
 
     public Creature(Coordinates coordinates, Map map) {
         super(coordinates, map);
+        this.hp = 1;
     }
 
-    protected final AStarPathSearch pathSearch = new AStarPathSearch();
 
-    public abstract void makeMove();
+    public void makeMove() {
+        List<Coordinates> pathToTarget = pathSearch.findPath(this, map);
+        int pathSize = pathToTarget.size();
+        Entity thisEntity = map.getEntity(this.getCoordinates());
+
+        if (pathSize == 0) {
+            map.replaceEntity(thisEntity, map.getNearbyEmptyCoordinates(thisEntity.getCoordinates()));
+            return;
+        }
+
+        int maxIndexOfPathList = pathSize - 1;
+
+        if (pathSize == 1) {
+            doSomething(pathToTarget.get(maxIndexOfPathList));
+            return;
+        }
+
+        if (this.getSpeed() >= pathSize) {
+            int indexOfPenultimatePathElement = pathSize - 2;
+            Coordinates coordinatesToMove = pathToTarget.get(indexOfPenultimatePathElement);
+            map.replaceEntity(thisEntity, coordinatesToMove);
+            doSomething(pathToTarget.get(maxIndexOfPathList));
+        } else {
+            Coordinates coordinatesToMove = pathToTarget.get(this.getSpeed() - 1);
+            map.replaceEntity(thisEntity, coordinatesToMove);
+        }
+    }
+
+    public abstract void doSomething(Coordinates targetCoordinates);
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
 }
