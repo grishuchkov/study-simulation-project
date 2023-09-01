0a74618eacb3fe68e3f67cc5b615c971b9454aac
grishuchkov <gri.d.a@yandex.ru>
2023-09-01 13:26:04 +0300

    add: start and inSimulation menu

--- src/main/java/org/simulation/Main.java
+++ src/main/java/org/simulation/Main.java
@@ -1,8 +1,61 @@
 package org.simulation;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    private static boolean acceptRules = false;
+
+    public static void main(String[] args) throws InterruptedException {
         Simulation simulation = new Simulation();
-        simulation.startSimulation();
+        Scanner scanner = new Scanner(System.in);
+
+
+        printGuide(scanner);
+        Thread menu = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String input = "";
+                System.out.println("Enter S for start");
+
+                while (true) {
+                    input = scanner.nextLine().toLowerCase();
+                    if ("s".equals(input)) {
+                        simulation.setPause(false);
+                    }
+                    if ("p".equals(input)) {
+                        simulation.setPause(true);
+                    }
+                    if ("n".equals(input)) {
+                        simulation.nextTurn();
+                    }
+                }
+            }
+        });
+
+        while (acceptRules) {
+            menu.start();
+            simulation.startSimulation();
+            menu.interrupt();
+            scanner.close();
+            System.exit(0);
+        }
+    }
+
+    private synchronized static void printGuide(Scanner scanner) {
+        System.out.println("For simulation control, you can enter on the keyboard: \n" +
+                "s - for start simulation. \n" +
+                "p - for pause simulation. \n" +
+                "n - for turn next step of simulation \n" +
+                "\n" +
+                "Input YES, if everything is understandable");
+
+
+        String input = "";
+        while (!acceptRules) {
+            input = scanner.nextLine().toLowerCase();
+            if ("yes".equals(input)) {
+                acceptRules = true;
+            }
+        }
     }
 }
